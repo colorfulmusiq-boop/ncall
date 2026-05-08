ef2d5ba6dc5ab4b29066fe7018f8950e04b6048e
colorfulmusiq-boop <colorfulmusiq@gmail.com>
2026-05-08 12:59:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ncall-v35';
-const CACHE_NAME = 'ncall-v35';
+const CACHE_NAME = 'ncall-v36';
+const CACHE_NAME = 'ncall-v36';
 
 self.addEventListener('install', e => {
   self.skipWaiting();

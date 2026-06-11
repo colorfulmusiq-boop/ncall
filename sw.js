dd9341f4cac678c91bc7d4d1a382f6ac16259b7c
colorfulmusiq-boop <colorfulmusiq@gmail.com>
2026-06-11 19:28:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,54 @@
-const CACHE_NAME = 'ncall-v76';
+const CACHE_NAME = 'ncall-v77';
+const CORE_FILES = [
+  '/waiter.html',
+  '/guest.html',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(CORE_FILES))
+      .catch(() => {})
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     ).then(() => clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(() => {});
+        }
+        return res;
+      })
+      .catch(() =>
+        caches.match(e.request, { ignoreSearch: true }).then(hit => {
+          if (hit) return hit;
+          if (e.request.mode === 'navigate') {
+            return caches.match('/waiter.html');
+          }
+          return Response.error();
+        })
+      )
+  );
 });
 
 self.addEventListener('push', e => {
